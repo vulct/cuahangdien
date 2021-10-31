8bd706918e43067bb5522ac32e35c35aeadf3ab1
Le Cong Tuan Vu <tuanvu237362@gmail.com>
2021-11-01 01:07:55 +0700

    customize path errors with request is admin

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\View;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +38,22 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Register the error template hint paths.
+     *
+     * @return void
+     */
+    protected function registerErrorViewPaths()
+    {
+        parent::registerErrorViewPaths();
+
+        if (request()->is('admin/*')) {
+            View::prependNamespace(
+                'errors',
+                realpath(base_path('resources/views/admin/errors'))
+            );
+        }
     }
 }
