658a2bc9aa95284772819b212eee6def129e8598
vulct <tuanvu237362@gmail.com>
2022-01-01 09:37:11 +0700

    update

---
+++ database/migrations/2021_12_26_092619_add_images_to_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddImagesToProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('image_01',255)->nullable()->default(null)->after('image');
+            $table->string('image_02',255)->nullable()->default(null)->after('image_01');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
